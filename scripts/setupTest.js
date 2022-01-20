8feda5d663105ef9cb3371cb6c2d87453ed1c9f1
Oscar Serna <oscr.serna@gmail.com>
2022-01-20 15:30:11 -0500

    github action

--- scripts/setupTest.js
+++ scripts/setupTest.js
@@ -1,1 +1,19 @@
 import "@testing-library/jest-dom/extend-expect"
+
+jest.mock("firebase/database", () => {
+    const actual = jest.requireActual("firebase/database")
+    return {
+        ...actual,
+        getDatabase: jest.fn(),
+    }
+})
+
+jest.mock("firebase/auth", () => {
+    const actual = jest.requireActual("firebase/auth")
+    return {
+        ...actual,
+        getAuth: jest.fn(() => ({
+            setPersistence: jest.fn(),
+        })),
+    }
+})
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,25 @@
 import { App } from "./App"
+import { ColumnsModal } from "./ColumnsModal"
+import { Content } from "./Content"
+import { DatePicker } from "./DatePicker"
+import { Header } from "./Header"
+import { PortfolioModal } from "./PortfolioModal"
+import { ProfitLoss } from "./ProfitLoss"
+import { SymbolModal } from "./SymbolModal"
+import { Table } from "./Table"
+import { TransactionModal } from "./TransactionModal"
+import { WeightingsModal } from "./WeightingsModal"
 
-export { App }
+export {
+    App,
+    ColumnsModal,
+    Content,
+    DatePicker,
+    Header,
+    PortfolioModal,
+    ProfitLoss,
+    SymbolModal,
+    Table,
+    TransactionModal,
+    WeightingsModal,
+}
