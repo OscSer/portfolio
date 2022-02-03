edc0ee3afd18e7f2ca0f130dc1b8ed873f655de3
Oscar Serna <oscr.serna@gmail.com>
2022-02-03 06:32:41 -0500

    add total profit

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -2,15 +2,18 @@ import "./Balance.scss"
 import { Utils } from "@domain"
 import { useTableData } from "@hooks"
 import React from "react"
+import { ProfitLoss } from "./ProfitLoss"
 
 const Balance = React.memo(function _Balance(): JSX.Element {
     const [data] = useTableData()
-    const { priceToString } = Utils
+    const { priceToString, percentToString } = Utils
 
     return (
         <div className="balance">
-            <h1>Balance</h1>
             <div className="balance__value">{priceToString(data.balance)}</div>
+            <ProfitLoss value={data.profit}>
+                {`${percentToString(data.profitPercent)} (${priceToString(data.profit)})`}
+            </ProfitLoss>
         </div>
     )
 })
--- src/hooks/useTableData.ts
+++ src/hooks/useTableData.ts
@@ -4,6 +4,13 @@ import { createGlobalState } from "react-use"
 type data = {
     tableData: TableData[]
     balance: number
+    profit: number
+    profitPercent: number
 }
-const useTableData = createGlobalState<data>({ tableData: [], balance: 0 })
+const useTableData = createGlobalState<data>({
+    tableData: [],
+    balance: 0,
+    profit: 0,
+    profitPercent: 0,
+})
 export { useTableData }
